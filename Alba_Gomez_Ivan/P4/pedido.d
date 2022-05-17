b54fc33d8e47320579f51aabc5d200b22221a686
IvanAlba97 <ivan02071997@gmail.com>
2022-05-17 23:16:22 +0200

    Comprimida la P4

--- Alba_Gomez_Ivan/P4/pedido.d
+++
@@ -1,2 +0,0 @@
-pedido.o pedido.d : pedido.cpp pedido.hpp usuario.hpp ../P1/cadena.hpp tarjeta.hpp \
-  articulo.hpp ../P1/fecha.hpp pedido-articulo.hpp usuario-pedido.hpp
--- Alba_Gomez_Ivan/P4/test-consola.d
+++
@@ -1,3 +0,0 @@
-test-consola.o test-consola.d : test-consola.cpp usuario.hpp ../P1/cadena.hpp tarjeta.hpp \
-  articulo.hpp ../P1/fecha.hpp pedido.hpp pedido-articulo.hpp \
-  usuario-pedido.hpp
